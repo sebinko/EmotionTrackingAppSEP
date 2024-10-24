30713e314d4cb5416e656b8272375055c353d5ce
sebinko <halas.sebastian@gmail.com>
2024-10-24 08:54:28 +0200

    add logging to emotions

--- JavaDAO/src/main/java/dk/via/JavaDAO/Protobuf/Emotions/EmotionsServiceImpl.java
+++ JavaDAO/src/main/java/dk/via/JavaDAO/Protobuf/Emotions/EmotionsServiceImpl.java
@@ -1,12 +1,17 @@
 package dk.via.JavaDAO.Protobuf.Emotions;
 
 import com.google.inject.Inject;
+import dk.via.JavaDAO.Main;
 import dk.via.JavaDAO.Protobuf.Emotions.EmotionsServiceGrpc.EmotionsServiceImplBase;
 import dk.via.JavaDAO.Services.EmotionListService;
 import io.grpc.stub.StreamObserver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class EmotionsServiceImpl extends EmotionsServiceImplBase {
   private final EmotionListService emotionListService;
+  private final Logger logger = LoggerFactory.getLogger(Main.class.getName());
+
 
   @Inject
   public EmotionsServiceImpl(EmotionListService emotionListService) {
@@ -25,8 +30,8 @@ public class EmotionsServiceImpl extends EmotionsServiceImplBase {
     } else if (request.hasField(EmotionsRequest.getDescriptor().findFieldByName("color"))) {
       handleFilterByColor(request, responseObserver);
     } else {
-      System.out.println("Getting all emotions");
-      // build emotions from list
+      logger.info("GetEmotionsMethod - GET ALL");
+
       EmotionsMessage.Builder builder = EmotionsMessage.newBuilder();
 
       emotionListService.getEmotions().forEach(e -> {
@@ -45,8 +50,11 @@ public class EmotionsServiceImpl extends EmotionsServiceImplBase {
   }
 
   private void handleSingleEmotion(EmotionsRequest request, StreamObserver<EmotionsMessage> responseObserver) {
+    logger.info("GetEmotionsMethod - GET SINGLE");
+
     String query = request.getEmotion();
     dk.via.JavaDAO.Models.Emotion emotion = emotionListService.getEmotion(query);
+
 
     if (emotion != null) {
       responseObserver.onNext(EmotionsMessage.newBuilder().addEmotions(Emotion.newBuilder().setEmotion(emotion.getEmotion()).setColor(emotion.getColor().toString()).setDescription(emotion.getDescription())).build());
@@ -59,6 +67,8 @@ public class EmotionsServiceImpl extends EmotionsServiceImplBase {
   }
 
   private void handleFilterByColor(EmotionsRequest request, StreamObserver<EmotionsMessage> responseObserver) {
+    logger.info("GetEmotionsMethod - GET BY COLOR");
+
     String query = request.getColor();
     dk.via.JavaDAO.Models.Color color = dk.via.JavaDAO.Models.Color.valueOf(query.toUpperCase());
 
