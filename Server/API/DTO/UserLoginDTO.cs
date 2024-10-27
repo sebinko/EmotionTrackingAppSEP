686c1b985cf36f0d33b06a4f03f6fc5a61549ef3
sebinko <halas.sebastian@gmail.com>
2024-10-27 14:09:58 +0100

    api auth implementation

---
+++ Server/API/DTO/UserLoginDTO.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTO;
+
+public class UserLoginDTO
+{
+  [Required]
+  public required string Username { get; set; }
+  [Required]
+  public required string Password { get; set; }
+  
+}
