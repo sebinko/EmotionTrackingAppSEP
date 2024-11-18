760b190d11219981558278d42365a40adcbed9a0
Sebastian Halas <halas.sebastian@gmail.com>
2024-11-18 10:05:29 +0100

    a bit of auth refactoring (#21)

--- Server/API/Controllers/EmotionCheckInsController.cs
+++ Server/API/Controllers/EmotionCheckInsController.cs
@@ -1,4 +1,6 @@
-﻿using API.DTO;
+﻿using System.Security.Claims;
+using API.DTO;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Protobuf.Services;
 
@@ -9,14 +11,24 @@ namespace API.Controllers;
 public class EmotionCheckInsController(EmotionCheckInService emotionCheckInService) : ControllerBase
 {
   [HttpPost]
+  [Authorize]
   public async Task<IActionResult> Create([FromBody] EmotionCheckInCreateDTO emotionCheckInDto)
   {
-    return Ok(await emotionCheckInService.Create(emotionCheckInDto));
+    var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+    
+    if (userId == null)
+    {
+      return Unauthorized();
+    }
+
+    return Ok(await emotionCheckInService.Create(emotionCheckInDto, int.Parse(userId)));
   }
-  
+
   [HttpDelete]
+  [Authorize]
   public async Task<IActionResult> Delete([FromBody] int id)
   {
+    // TODO CHECK IF IT BELONGS TO THE USER
     return Ok(await emotionCheckInService.Delete(id));
   }
 }
