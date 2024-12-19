e761da5941b7d994512a47cf913482ec96426a2c
tomaspsotkka <psotka439@gmail.com>
2024-12-19 21:57:51 +0100

    TagsDAODBIntegrationTests fix

--- JavaDAO/src/test/java/dk/via/JavaDAO/DAO/TagsDAODBIntegrationTests.java
+++ JavaDAO/src/test/java/dk/via/JavaDAO/DAO/TagsDAODBIntegrationTests.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TagsDAODBIntegrationTests {
-  private TagsDAODB tagsDAODB;
+  private TagsDAO tagsDAO;
   private DBConnector dbConnector;
   private Connection connection;
 
@@ -26,87 +26,87 @@ public class TagsDAODBIntegrationTests {
   void setUp() throws SQLException {
     dbConnector = new PostgresConnector(new AppConfig());
     connection = dbConnector.getConnection();
-    tagsDAODB = new TagsDAODB(dbConnector);
+    tagsDAO = new TagsDAODB(dbConnector);
   }
 
   @Test
   void testCreateAndAssignTag() throws SQLException {
-    Tag tag = new Tag(0, "testKey", TagType.WHAT, null, null, 19);
-    EmotionCheckIn checkIn = new EmotionCheckIn(0, "Happy", "Feeling good", null, null, 1);
-    tagsDAODB.AssignTag(tag, checkIn);
+    Tag tag = new Tag(1, "testKey", TagType.WHAT, null, null, 1);
+    EmotionCheckIn checkIn = new EmotionCheckIn(1, "Happy", "Feeling good", null, null, 1);
+    tagsDAO.AssignTag(tag, checkIn);
 
-    List<Tag> tags = tagsDAODB.GetAllForCheckIn(checkIn);
+    List<Tag> tags = tagsDAO.GetAllForCheckIn(checkIn);
     assertNotNull(tags);
   }
 
   @Test
   void testGetSingleTagById() throws SQLException {
-    Tag tag = new Tag(1, "testKey", TagType.WHAT, null, null, 1);
-    tagsDAODB.AssignTag(tag, new EmotionCheckIn(0, "Happy", "Feeling good", null, null, 19));
+    Tag tag = new Tag(1, "Home", TagType.WHAT, null, null, 1);
+    tagsDAO.AssignTag(tag, new EmotionCheckIn(1, "Happy", "Feeling good", null, null, 1));
 
-    Tag fetchedTag = tagsDAODB.GetSingle(51);
+    Tag fetchedTag = tagsDAO.GetSingle(1);
     assertNotNull(fetchedTag);
-    assertEquals("testKey", fetchedTag.getKey());
+    assertEquals("Home", fetchedTag.getKey());
   }
 
   @Test
   void testGetSingleTagByKeyTypeAndUserId() throws SQLException {
-    Tag tag = new Tag(0, "testKey", TagType.WHAT, null, null, 19);
-    tagsDAODB.AssignTag(tag, new EmotionCheckIn(0, "Happy", "Feeling good", null, null, 19));
+    Tag tag = new Tag(1, "Home", TagType.WHAT, null, null, 1);
+    tagsDAO.AssignTag(tag, new EmotionCheckIn(1, "Happy", "Feeling good", null, null, 1));
 
-    Tag fetchedTag = tagsDAODB.GetSingle("testKey", TagType.WHAT, 19);
+    Tag fetchedTag = tagsDAO.GetSingle("Home", TagType.WHAT, 1);
     assertNotNull(fetchedTag);
-    assertEquals("testKey", fetchedTag.getKey());
+    assertEquals("Home", fetchedTag.getKey());
   }
 
   @Test
   void testGetAllTagsForCheckIn() throws SQLException {
     Tag tag1 = new Tag(1, "testKey1", TagType.WHAT, null, null, 1);
     Tag tag2 = new Tag(2, "testKey2", TagType.WHAT, null, null, 1);
-    EmotionCheckIn checkIn = new EmotionCheckIn(0, "Happy", "Feeling good", null, null, 19);
-    tagsDAODB.AssignTag(tag1, checkIn);
-    tagsDAODB.AssignTag(tag2, checkIn);
+    EmotionCheckIn checkIn = new EmotionCheckIn(1, "Happy", "Feeling good", null, null, 1);
+    tagsDAO.AssignTag(tag1, checkIn);
+    tagsDAO.AssignTag(tag2, checkIn);
 
-    List<Tag> tags = tagsDAODB.GetAllForCheckIn(checkIn);
+    List<Tag> tags = tagsDAO.GetAllForCheckIn(checkIn);
     assertNotNull(tags);
   }
 
   @Test
   void testGetAllTagsForUser() throws SQLException {
-    Tag tag1 = new Tag(1, "testKey1", TagType.WHAT, null, null, 19);
-    Tag tag2 = new Tag(2, "testKey2", TagType.WHAT, null, null, 19);
-    tagsDAODB.AssignTag(tag1, new EmotionCheckIn(0, "Happy", "Feeling good", null, null, 19));
-    tagsDAODB.AssignTag(tag2, new EmotionCheckIn(0, "Happy", "Feeling great", null, null, 19));
+    Tag tag1 = new Tag(1, "testKey1", TagType.WHAT, null, null, 1);
+    Tag tag2 = new Tag(2, "testKey2", TagType.WHAT, null, null, 1);
+    tagsDAO.AssignTag(tag1, new EmotionCheckIn(1, "Happy", "Feeling good", null, null, 1));
+    tagsDAO.AssignTag(tag2, new EmotionCheckIn(1, "Happy", "Feeling great", null, null, 1));
 
-    List<Tag> tags = tagsDAODB.GetAllForUser(19);
+    List<Tag> tags = tagsDAO.GetAllForUser(1);
     assertNotNull(tags);
   }
 
   @Test
   void testRemoveTag() throws SQLException {
-    Tag tag = new Tag(1, "testKey", TagType.WHAT, null, null, 19);
-    EmotionCheckIn checkIn = new EmotionCheckIn(0, "Happy", "Feeling good", null, null,19);
-    tagsDAODB.AssignTag(tag, checkIn);
+    Tag tag = new Tag(1, "testKey", TagType.WHAT, null, null, 1);
+    EmotionCheckIn checkIn = new EmotionCheckIn(1, "Happy", "Feeling good", null, null,1);
+    tagsDAO.AssignTag(tag, checkIn);
 
-    tagsDAODB.RemoveTag(tag, checkIn);
+    tagsDAO.RemoveTag(tag, checkIn);
 
-    List<Tag> tags = tagsDAODB.GetAllForCheckIn(checkIn);
+    List<Tag> tags = tagsDAO.GetAllForCheckIn(checkIn);
     assertNotNull(tags.isEmpty());
   }
 
   @Test
   void testCreateTagWithInvalidData() {
     Tag invalidTag = new Tag(null, null, null, null, null, null);
-    EmotionCheckIn checkIn = new EmotionCheckIn(0, "Happy", "Feeling good", null, null, 19);
+    EmotionCheckIn checkIn = new EmotionCheckIn(1, "Happy", "Feeling good", null, null, 1);
     assertThrows(NullPointerException.class, () -> {
-      tagsDAODB.AssignTag(invalidTag, checkIn);
+      tagsDAO.AssignTag(invalidTag, checkIn);
     });
   }
 
   @Test
   void testGetTagsForNonExistentEmotionCheckIn() throws SQLException {
     EmotionCheckIn nonExistentCheckIn = new EmotionCheckIn(999, "NonExistent", "NonExistent", null, null, 19);
-    List<Tag> tags = tagsDAODB.GetAllForCheckIn(nonExistentCheckIn);
+    List<Tag> tags = tagsDAO.GetAllForCheckIn(nonExistentCheckIn);
     assertTrue(tags.isEmpty());
   }
 }
